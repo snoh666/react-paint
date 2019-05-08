5e144249cc857e41c2a7ecc9e8ef3eedde324f64
Mikolaj Snochowski <snochopl@gmail.com>
2019-05-08 16:25:57 +0200

    Updates

---
+++ precache-manifest.9bc7a25c0b449474f017bc2ee9b54642.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6655400f5e4049bc0e1b6eac355a70e9",
+    "url": "/react-snake-game/index.html"
+  },
+  {
+    "revision": "d2a0f156a344fc0fe1a6",
+    "url": "/react-snake-game/static/css/main.691d5104.chunk.css"
+  },
+  {
+    "revision": "002394b6a5e6ff327509",
+    "url": "/react-snake-game/static/js/2.1714a347.chunk.js"
+  },
+  {
+    "revision": "d2a0f156a344fc0fe1a6",
+    "url": "/react-snake-game/static/js/main.79e7319c.chunk.js"
+  },
+  {
+    "revision": "2bb4e33f074ce70d55a3",
+    "url": "/react-snake-game/static/js/runtime~main.87448e3a.js"
+  }
+]);
