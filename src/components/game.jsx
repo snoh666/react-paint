45a30798ea6f21252ff7641ae85425c0e6c2f3d8
Mikolaj Snochowski <snochopl@gmail.com>
2019-05-09 20:23:18 +0200

    Give it up on this

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,15 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// const Setup = React.forwardRef((props, ref) => (
+//   <div className="app-content">
+//     <h1>Game</h1>
+//     <Link to='/'>
+//       <button className="exit-btn">Go Home</button>
+//     </Link>
+//     <canvas ref={ref}></canvas>
+//   </div>
+// ));
 const Game = () => {
-  return (
-    <div className="app-content">
-      <h1>Game</h1>
-      <Link to='/'>
-        <button className="exit-btn">Go Home</button>
-      </Link>
-    </div>
+  let canvasRef = React.createRef();
+  /*
+    canvas size:
+      width: 1000px
+      height: 500px
 
+  */
+  const start = () => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+    const setup = {
+      screenX: 1000,
+      screenY: 500,
+      playerX: 1000 / 2 - 10,
+      playerY: 500 / 2 -10
+    };
+    console.log(setup);
+
+    ctx.beginPath();
+    ctx.strokeRect(setup.playerX, setup.playerY,20,20);
+    ctx.stroke();
+  };
+  return (
+    (
+      <div className="app-content">
+        <h1>Game</h1>
+        <Link to='/'>
+          <button className="exit-btn">Go Home</button>
+        </Link>
+        <p>Click on canvas to play!</p>
+        <canvas
+          ref={canvasRef}
+          onClick={start}
+        />
+      </div>
+    )
   );
 };
 
