e612570ac41168180f5bdb9e0f5bc9c8493947e6
Mikolaj Snochowski <snochopl@gmail.com>
2019-05-19 00:30:30 +0200

    Updated h1 and description

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,9 +5,9 @@ const Home = () => {
   return (
     <div className="app-content">
       <div className="home-header">
-        <h1>Canvas Vanilla Js Snake game</h1>
+        <h1>Canvas Vanilla JavaScript Paint</h1>
         <div className="description">
-          <span>It's a basic no textured snake game. Created for fun.</span>
+          <span>Paint in Vanilla JavaScript. Created for fun in free time by <a href="https://github.com/snoh666/">snoh666</a>.</span>
         </div>
       </div>
       <Link to='/game/'>
