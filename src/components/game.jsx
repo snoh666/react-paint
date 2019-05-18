4f1bc8825df2231dd1900f0d7bb1b2ae9ee4bb97
Mikolaj Snochowski <snochopl@gmail.com>
2019-05-19 00:31:00 +0200

    MVP paint

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -5,39 +5,40 @@ class Game extends React.Component{
 
   componentDidMount() {
     const canvas = this.refs.canvas;
-    const ctx = canvas.getContext('2d');
-
     canvas.width = canvas.clientWidth;
     canvas.height = canvas.clientHeight;
 
-    ctx.strokeColor = 'black';
-    ctx.strokeRect(20, 20, 10, 10);
-    console.log('stroked');
-
-    this.startGame(canvas, ctx);
-
-    // document.addEventListener('keydown', e => console.log(e));
+    this._startGame();
   }
 
-  startGame(canvas, ctx) {
-    console.log('Hello from startGame');
+  _startGame() {
+    const canvas = this.refs.canvas;
+    const ctx = canvas.getContext('2d');
+    console.log(canvas.getBoundingClientRect());
+    const canvasPos = [canvas.getBoundingClientRect().x.toFixed(), 213];
 
-    ctx.strokeRect(canvas.width / 2 - 10, canvas.height / 2 - 10, 20, 20);
+    document.onmousemove = (e) => {
+      if(e.screenX > canvasPos[0] && e.screenY > canvasPos[1] && e.ctrlKey !== true){
+        ctx.fillRect(e.screenX - canvasPos[0] - 10, e.screenY - canvasPos[1] - 10, 20, 20);
+      }
+    };
   }
 
-
+  componentWillUnmount() {
+    document.onmousemove = () => {};
+  }
 
   render() {
     return (
       <div className="app-content">
-        <h1>Game</h1>
+        <h1>Paint</h1>
         <Link to='/'>
           <button className="exit-btn">Go Home</button>
         </Link>
-        <p>Click on canvas to play!</p>
+        <p>Move mouse on canvas to draw! Hold ctrl to move without drawing.</p>
         <canvas
           ref='canvas'
-          style={{height: window.innerHeight / 3 * 2, width: window.innerWidth - 50}}
+          style={{height: 481, width: window.innerWidth - 50, backgroundColor: 'white'}}
         />
       </div>
     );
