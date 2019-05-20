e3abba0d4eee7299dfe47aa17bae33c12d712057
Mikolaj Snochowski <snochopl@gmail.com>
2019-05-20 19:54:54 +0200

    Created ul list with controls moved from game component

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,7 +7,15 @@ const Home = () => {
       <div className="home-header">
         <h1>Canvas Vanilla JavaScript Paint</h1>
         <div className="description">
-          <span>Paint in Vanilla JavaScript. Created for fun in free time by <a href="https://github.com/snoh666/">snoh666</a>.</span>
+          <span>
+            Paint in Vanilla JavaScript. Created for fun in free time by <a href="https://github.com/snoh666/">snoh666</a>.
+            <ul>
+              <h2>Controls:</h2>
+              <li>+ numpad - Increase size of brush</li>
+              <li>- numpad - Decrease size of brush</li>
+              <li>LeftCtrl - Clear canvas</li>
+            </ul>
+          </span>
         </div>
       </div>
       <Link to='/game/'>
